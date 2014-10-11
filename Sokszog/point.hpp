cb899132f1fdb135eac8d280419c1dfe07ce1b95
firstvan <firstvan@live.com>
2014-10-11 18:38:19 +0200

    Full commented

--- Sokszog/point.hpp
+++ Sokszog/point.hpp
@@ -45,10 +45,12 @@ public:
 		y = rhsy;
 	}
 
+	//x növelése paraméterrel
 	void addX(T val){
 		x += val;
 	}
 
+	//y növelése paraméterrel
 	void addY(T val){
 		y += val;
 	}
@@ -60,27 +62,13 @@ public:
 		glEnd();
 	}
 
-	void incX(){
-		x++;
-	}
-
-	void incY(){
-		y++;
-	}
-
-	void decX(){
-		x--;
-	}
-
-	void decY(){
-		y--;
-	}
-
+	//x y növelése paraméterrel
 	void add(T rhsX, T rhsY){
 		x += rhsX;
 		y += rhsY;
 	}
 
+	//x y csökkentése paraméterrel
 	void sub(T rhsX, T rhsY){
 		x -= rhsX;
 		y -= rhsY;
@@ -103,6 +91,7 @@ public:
 		return myPoint(this->getX() + rhs.getX(), this->getY() + rhs.getY());
 	}
 
+	//pont távolsága négyzete a paraméterben meghatározott két pont által határolt egyenesnek
 	double lineDistance2(const myPoint& a1, const myPoint& a2){
 		GLdouble x = a1.getY() - a2.getY();
 		GLdouble y = a2.getX() - a1.getX();
@@ -111,7 +100,7 @@ public:
 		return pow(fabs(x * this->getX() + y * this->getY() - c), 2) / (x * x + y * y);
 	}
 
-
+	//pont távolsága 2pont által határole egyeneseknek
 	double lineDistance(const myPoint& a1, const myPoint& a2){
 		GLdouble x = a1.getY() - a2.getY();
 		GLdouble y = a2.getX() - a1.getX();
@@ -120,12 +109,12 @@ public:
 		return abs(x * this->getX() + y * this->getY() - c) / sqrt(x * x + y * y);
 	}
 
+	//két pont távolság négyzete
 	double pointDis2(const myPoint & p){
 		GLdouble retX = x - p.getX();
 		GLdouble retY = y - p.getY();
 		return (retX * retX + retY * retY);
 	}
-
 
 };
 
