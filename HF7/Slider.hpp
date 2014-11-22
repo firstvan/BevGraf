cba63dc3a5b0ece8398ccb6f39ec9448750d9caa
firstvan <firstvan@live.com>
2014-11-22 01:11:34 +0100

    done2

---
+++ HF7/Slider.hpp
@@ -0,0 +1,191 @@
+#include "point.hpp"
+#include <vector>
+#include <cmath>
+
+template<typename T>
+class mySlider
+{
+    myPoint<T> p1;
+    myPoint<T> p2;
+    T part;
+    myPoint<T> click;
+    std::vector<myPoint<T>> points;
+    GLint current = 0;
+    T start = 0, end = 0, interval = 0;
+    T value = 0;
+
+
+public:
+
+    mySlider<T>()
+    {
+        p1 = new myPoint<T>();
+        p2 = new myPoint<T>();
+        part = 0;
+        click = new myPoint<T>();
+    }
+
+    mySlider<T>(myPoint<T> rp1, myPoint<T> rp2, T pr = 0)
+    {
+        p1 = rp1;
+        p2 = rp2;
+        part = pr;
+        click = p1;
+        T x = sqrt(p1.pointDis2(p2)) / part;
+
+
+        if (rp2.getY() - rp1.getY() == 0)
+        {
+            for (int i = 1; i < part; i++)
+            {
+                points.push_back(myPoint<T>(p1.getX() + i * x, p1.getY()));
+            }
+        }
+        else if (rp2.getX() - rp1.getX() == 0)
+        {
+            for (int i = 1; i < part; i++)
+            {
+                points.push_back(myPoint<T>(p1.getX() , p1.getY() + i * x));
+            }
+        }
+
+    }
+
+    mySlider<T>(myPoint<T> rp1, myPoint<T> rp2, T strt, T nd, T pr = 0)
+    {
+        p1 = rp1;
+        p2 = rp2;
+        part = pr;
+        click = p1;
+        start = strt;
+        end = nd;
+        interval = end - start;
+
+        GLdouble x = static_cast<GLdouble>(sqrt(p1.pointDis2(p2)) / part);
+
+        if (rp2.getY() - rp1.getY() == 0)
+        {
+            for (int i = 1; i < part; i++)
+            {
+                points.push_back(myPoint<T>(static_cast<T>(p1.getX() + i * x), p1.getY()));
+            }
+        }
+        else if (rp2.getX() - rp1.getX() == 0)
+        {
+            for (int i = 1; i < part; i++)
+            {
+                points.push_back(myPoint<T>(p1.getX(), static_cast<T>(p1.getY() + i * x)));
+            }
+        }
+
+    }
+
+
+
+    myPoint<T> getClick() const
+    {
+        return click;
+    }
+
+    GLint getCurrentPosition()
+    {
+        return current + start;
+    }
+
+    T getValue()
+    {
+        return value;
+    }
+
+    void setClickX(T rhs)
+    {
+        if (rhs > p1.getX() && rhs < p2.getX())
+        {
+            if (part == 0)
+            {
+                click.setX(rhs);
+                current = static_cast<GLint>(rhs);
+                double x = std::round(static_cast<double>(click.getX() - p1.getX()) / ((p2.getX() -p1.getX()) / static_cast<double>(interval))) + start;
+                value = static_cast<T>(x);
+            }
+            else
+            {
+                GLdouble mind = abs(p1.getX() - rhs);
+                GLint elem = -1;
+
+                if (mind > abs(p2.getX() - rhs))
+                {
+                    mind = abs(p2.getX() - rhs);
+                    elem = static_cast<GLint>(part + 1);
+                }
+
+                for (int i = 0; i < part - 1; i++)
+                {
+                    if (mind > abs(points[i].getX() - rhs))
+                    {
+                        mind = abs(points[i].getX() - rhs);
+                        elem = i;
+                    }
+                }
+
+                if (elem == -1)
+                {
+                    T temp = p1.getX();
+                    click.setX(temp);
+                    current = 0;
+                }
+                else if (elem == part + 1)
+                {
+                    T temp = p2.getX();
+                    click.setX(temp);
+                    current = part;
+                }
+                else
+                {
+                    T temp = points[elem].getX();
+                    click.setX(temp);
+                    current = elem + 1;
+                }
+            }
+        }
+    }
+
+    void inline draw()
+    {
+
+        if (part > 1)
+        {
+            glPointSize(3);
+            glBegin(GL_POINTS);
+            for (int i = 0; i < part-1; i++)
+            {
+                glVertex2d(points[i].getX(), points[i].getY());
+            }
+            glEnd();
+
+        }
+
+        glBegin(GL_LINES);
+        glVertex2d(p1.getX(),p1.getY());
+        glVertex2d(p2.getX(), p2.getY());
+        glEnd();
+
+        glColor3f(0.0, 1.0, 0.0);
+        glPointSize(5);
+        glBegin(GL_POINTS);
+        glVertex2d(p1.getX(), p1.getY());
+        glVertex2d(p2.getX(), p2.getY());
+        glEnd();
+
+        glColor3f(0.0, 0.0, 1.0);
+        glPointSize(10);
+        glBegin(GL_POINTS);
+        glVertex2d(click.getX(), click.getY());
+        glEnd();
+    }
+
+    GLdouble dis2Click(T a1, T a2)
+    {
+        return (a1 - click.getX()) * (a1 - click.getX()) + (a2 - click.getY()) * (a2 - click.getY());
+    }
+};
---
+++ HF7/point.hpp
@@ -0,0 +1,147 @@
+﻿#ifndef POINTS_HPP
+#define POINTS_HPP
+
+#include <GL/glut.h>
+#include <math.h>
+
+
+template<typename T>
+class myPoint
+{
+    T x, y;
+
+public:
+    //pont inicializálása
+    myPoint() : x(0), y(0) {}
+
+    //pont létrehozása x,y pontban
+    myPoint(T rhsX, T rhsY) : x(rhsX), y(rhsY)
+    {}
+
+    //másoló konstruktor
+    myPoint(const myPoint<T>& rhs)
+    {
+        x = rhs.getX();
+        y = rhs.getY();
+    }
+
+
+
+    //x visszatérítése
+    T getX() const
+    {
+        return x;
+    }
+
+    //y visszatérítése
+    T getY() const
+    {
+        return y;
+    }
+
+    //x beállítása
+    void setX(const T& rhsx)
+    {
+        x = rhsx;
+    }
+
+    //y beállytása
+    void setY(const T& rhsy)
+    {
+        y = rhsy;
+    }
+
+    //x növelése paraméterrel
+    void addX(T val)
+    {
+        x += val;
+    }
+
+    //y növelése paraméterrel
+    void addY(T val)
+    {
+        y += val;
+    }
+
+    //pont kirajzolása
+    void display() const
+    {
+        glBegin(GL_POINTS);
+        glVertex2d(x, y);
+        glEnd();
+    }
+
+    //x y növelése paraméterrel
+    void add(T rhsX, T rhsY)
+    {
+        x += rhsX;
+        y += rhsY;
+    }
+
+    //x y csökkentése paraméterrel
+    void sub(T rhsX, T rhsY)
+    {
+        x -= rhsX;
+        y -= rhsY;
+    }
+
+    // pontok egyenlõség vizsgálata
+    bool operator==(myPoint& rhs) const
+    {
+        if (this->getX() == rhs.getX() && this->getY() == rhs.getY())
+            return true;
+        return false;
+    }
+
+    //pont számmal való szorzása
+    myPoint operator*(const double rhs) const
+    {
+        return myPoint(rhs*this->getX(), rhs*this->getY());
+    }
+
+    //pontok összeadása
+    myPoint operator+(const myPoint& rhs)
+    {
+        return myPoint(this->getX() + rhs.getX(), this->getY() + rhs.getY());
+    }
+
+    //pont távolsága négyzete a paraméterben meghatározott két pont által határolt egyenesnek
+    double lineDistance2(const myPoint& a1, const myPoint& a2)
+    {
+        GLdouble x = a1.getY() - a2.getY();
+        GLdouble y = a2.getX() - a1.getX();
+        GLdouble c = (a1.getY() - a2.getY()) * a1.getX() + (a2.getX() - a1.getX()) * a1.getY();
+
+        return pow(fabs(x * this->getX() + y * this->getY() - c), 2) / (x * x + y * y);
+    }
+
+    //pont távolsága 2pont által határole egyeneseknek
+    double lineDistance(const myPoint& a1, const myPoint& a2)
+    {
+        GLdouble x = a1.getY() - a2.getY();
+        GLdouble y = a2.getX() - a1.getX();
+        GLdouble c = (a1.getY() - a2.getY()) * a1.getX() + (a2.getX() - a1.getX()) * a1.getY();
+
+        return abs(x * this->getX() + y * this->getY() - c) / sqrt(x * x + y * y);
+    }
+
+    //két pont távolság négyzete
+    double pointDis2(const myPoint & p)
+    {
+        GLdouble retX = x - p.getX();
+        GLdouble retY = y - p.getY();
+        return (retX * retX + retY * retY);
+    }
+
+    double pointDis2(T x1, T y1)
+    {
+        GLdouble retX = x - x1;
+        GLdouble retY = y - y1;
+        return (retX * retX + retY * retY);
+    }
+
+};
+
+
+
+#endif
